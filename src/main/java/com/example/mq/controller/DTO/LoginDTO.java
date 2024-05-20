6156d08f23a08aae79c0123109a2b149262a5626
fregnani2 <brunofregnani665@gmail.com>
2024-05-20 15:32:29 -0300

    feat: token & login DTO for controller

---
+++ src/main/java/com/example/mq/controller/DTO/LoginDTO.java
@@ -0,0 +1,8 @@
+package com.example.mq.controller.DTO;
+
+/**
+ * DTO for login
+ */
+public record LoginDTO(String email, String password) {
+
+}
---
+++ src/main/java/com/example/mq/controller/DTO/TokenDTO.java
@@ -0,0 +1,7 @@
+package com.example.mq.controller.DTO;
+
+/**
+ * DTO for token
+ */
+public record TokenDTO (String token){
+}
