dee2e45816b49a8fd1d2a6b6f61f3ee4b829a4a4
fregnani2 <brunofregnani665@gmail.com>
2024-05-20 23:01:32 -0300

    chore: remove unused imports

--- src/test/java/com/example/mq/service/ClientServiceTest.java
+++ src/test/java/com/example/mq/service/ClientServiceTest.java
@@ -14,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
--- src/test/java/com/example/mq/service/WorkerServiceTest.java
+++ src/test/java/com/example/mq/service/WorkerServiceTest.java
@@ -12,7 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
